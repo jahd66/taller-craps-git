35e77ac604574cf9a6d4fd0cd2e3554526302f5f
Jesús <jesusandreshernandez66@gmail.com>
2024-10-12 03:06:11 -0500

    Implementé los requerimientos del estudiante #1

--- src/main/java/org/example/crapsgame/controller/GameController.java
+++ src/main/java/org/example/crapsgame/controller/GameController.java
@@ -7,30 +7,44 @@ import javafx.scene.image.ImageView;
 import org.example.crapsgame.model.Dice;
 import org.example.crapsgame.model.Game;
 
-import java.util.ArrayList;
-
 public class GameController {
 
     @FXML
-    private Label pointLabel, shootLabel;
+    private Label pointLabel, shootLabel, resultLabel, gamesWonLabel, gamesLostLabel;
 
     @FXML
     private ImageView dice1ImageView, dice2ImageView;
 
-    Dice dice1, dice2;
-    ArrayList<Game> games = new ArrayList<Game>();
+    private Game game;
 
     public GameController() {
-        this.dice1 = new Dice();
-        this.dice2 = new Dice();
+        this.game = new Game();
     }
 
     @FXML
     public void onHandleButtonRollTheDice(ActionEvent event) {
-        this.dice1.rollDice();
-        this.dice2.rollDice();
-        this.dice1ImageView.setImage(this.dice1.getDiceImage());
-        this.dice2ImageView.setImage(this.dice2.getDiceImage());
-    }
+        // Lanza los dados de la clase Game
+        game.rollDices();
 
+        // Actualiza las imágenes de los dados
+        this.dice1ImageView.setImage(game.dice1.getDiceImage());
+        this.dice2ImageView.setImage(game.dice2.getDiceImage());
+
+        // Actualiza las etiquetas con los valores de tiro y punto
+        shootLabel.setText(String.valueOf(game.getShoot()));
+        pointLabel.setText(String.valueOf(game.getPoint()));
+
+        // Muestra si el jugador ganó o perdió
+        if (game.isWin()) {
+            resultLabel.setText("¡Ganaste!");
+        } else if (game.isLose()) {
+            resultLabel.setText("¡Perdiste!");
+        } else {
+            resultLabel.setText("Continúa jugando...");
+        }
+
+        // Actualiza los contadores de juegos ganados y perdidos
+        gamesWonLabel.setText(String.valueOf(game.getGamesWon()));
+        gamesLostLabel.setText(String.valueOf(game.getGamesLost()));
+    }
 }
